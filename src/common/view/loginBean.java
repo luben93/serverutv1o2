085b044a37cecb35bf4ae768a48c7688a672a30b
luben <luben93@gmail.com>
2015-12-05 22:05:55 +0100

    småsaker

--- src/common/view/loginBean.java
+++ src/common/view/loginBean.java
@@ -1,11 +1,19 @@
 package common.view;
 
-import common.bo.*;
+import common.bo.FriendHandler;
+import common.bo.ProfileHandler;
+import common.bo.WallHandler;
+import common.bo.chatHandler;
 import common.viewModel.*;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -24,12 +32,15 @@ public class loginBean {
     private Long id;
     private String post;
     private String msg;
-    private String searchName="";
+    private String searchName = "";
     private profile me;
     private profile other;//?????????
+    private WebTarget target;
 
-
-
+    public loginBean(){
+        Client cli = ClientBuilder.newClient();
+        target = cli.target("ip:8081/rest/");
+    }
 
     public String getMsg() {
         return msg;
@@ -51,68 +62,55 @@ public class loginBean {
         return pass;
     }
 
-//    public long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
+    public String getSearchName() {
+        return searchName;
+    }
+
+    public profile getShowProfile() {
+        return other;
+    }
 
     public void setPost(String post) {
         this.post = post;
     }
 
-    public String getPost(){
+    public String getPost() {
         return post;
     }
 
     public void setPass(String pass) {
         this.pass = pass;
     }
-
-
-
-
-    //TODO here
-//    public String getProfileName() {
-//        return me.getName();
-//    }
-//    public String getAge() {
-//        return me.getAge()+"";
-//    }
-//    public String getDesc() {
-//        return me.getDesc();
-//    }
-//    public String getGender() {
-//        me.getGender();
-//    }
-    public Collection<post> getWall(){
-        List<post> out;
-        out=WallHandler.getPosts(id);
-        Collections.reverse(out);
-        return out;
-
-    }
-
-    public profile getMe(){
+    public profile getMe() {
         return me;
     }
 
-    public Collection<post> getOtherWall(){
+    public void setSearchName(String name) {
+        searchName = name;
+    }
+
+    //TODO implementera rest härifrån och ner
+
+    public Collection<post> getWall() {
         List<post> out;
-        out=WallHandler.getPosts(other.getUid());
+        out = WallHandler.getPosts(id);
         Collections.reverse(out);
         return out;
 
     }
-    public void setSearchName(String name){
-        searchName = name;
+
+
+    public Collection<post> getOtherWall() {
+        List<post> out;
+        out = WallHandler.getPosts(other.getUid());
+        Collections.reverse(out);
+        return out;
+
     }
 
+
     public String postToWall() {
-        //do post
-        WallHandler.post(new post(id,post));//TODO nullpointer
+        WallHandler.post(new post(id, post));//TODO nullpointer
         post = "";
         return "home";
     }
@@ -124,69 +122,65 @@ public class loginBean {
     }
 
 
-    public Collection<message> getMessages(){
+    public Collection<message> getMessages() {
         List<message> out;
-        message between = new message(id,other.getUid());
-        out=chatHandler.getMessages(between);
+        message between = new message(id, other.getUid());
+        out = chatHandler.getMessages(between);
         Collections.reverse(out);
         return out;
     }
 
 
-    public void sendMessage (){
-        message out = new message(id,other.getUid(),msg);
+    public void sendMessage() {
+        message out = new message(id, other.getUid(), msg);
         chatHandler.sendMessage(out);
     }
 
     public Collection<profile> getResults() {
         if (searchName.equals("")) {
             return new ArrayList<profile>();
-        }else{
-           return ProfileHandler.search(new profile(searchName),me);
+        } else {
+            return ProfileHandler.search(new profile(searchName), me);
         }
     }
 
-    public String showThisProfile(long otherID){
+    public String showThisProfile(long otherID) {
         other = getProfile(otherID);
         setSearchName("");
         return "profileInfo";
     }
 
-    private profile getProfile(long id){
+    private profile getProfile(long id) {
         return ProfileHandler.getProfile(id);
     }
 
-    public String getnFollowers(){
-        return FriendHandler.getFollowers(id).size()+"";
-    }
-    public String getnFollowing(){
-        return FriendHandler.countFollowing(id)+"";
+    public String getnFollowers() {
+        return FriendHandler.getFollowers(id).size() + "";
     }
 
-    public String getSearchName(){
-        return searchName;
+    public String getnFollowing() {
+        return FriendHandler.countFollowing(id) + "";
     }
 
-    public profile getShowProfile() {
-        return other;
+
+    public String getOthernFollowers() {
+        return FriendHandler.getFollowers(other.getUid()).size() + "";
     }
 
-    public String getOthernFollowers(){
-        return FriendHandler.getFollowers(other.getUid()).size()+"";
-    }
-
-    public String getOthernFollowing(){
-        return FriendHandler.countFollowing(other.getUid())+"";
+    public String getOthernFollowing() {
+        WebTarget wt = target.path("following");
+        Response resp = wt.request().post(Entity.json(other.getUid()));
+        return resp.getEntity().toString();
     }
 
     public String addFriend(long otherID) {
         System.out.println(otherID);
         //TODO add friend here
-        Follower follow =new Follower(id,otherID);
-        FriendHandler.addFollower(follow);
-//        Client cli = ClientBuilder.newClient();
-//        WebTarget wt = cli.target("ip:port/path/rest/addFriend");
-//        Response resp = wt.request().post(Entity.json(follow));
+        Follower follow = new Follower(id, otherID);
+        WebTarget wt = target.path("friends");
+        Response resp = wt.request().post(Entity.json(follow));
+        //FriendHandler.addFollower(follow);
+
         return "home";//mabey profile/uid here?
     }
 
@@ -201,18 +195,18 @@ public class loginBean {
 //        }
 //        //TODO not logged in
 //        return "index";
-//        Client cli = ClientBuilder.newClient();
-//        WebTarget wt = cli.target("ip:port/path/rest/login");
-        ViewUser user = new ViewUser(username,pass);
-        long tmp = UserHandler.login(user);
-//        Response resp = wt.request().post(Entity.json(user));
-//        System.out.println(resp);
-//        System.out.println(resp.getStatus());
-//        System.out.println(resp.getEntity());
-//        long tmp=Long.valueOf(resp.getEntity().toString());
 
-        if(tmp > 0){//success
-            id=tmp;
+        WebTarget wt = target.path("login");
+        ViewUser user = new ViewUser(username, pass);
+//        long tmp = UserHandler.login(user);
+        Response resp = wt.request().post(Entity.json(user));
+        System.out.println(resp);
+        System.out.println(resp.getStatus());
+        System.out.println(resp.getEntity());
+        long tmp = Long.valueOf(resp.getEntity().toString());
+
+        if (tmp > 0) {//success
+            id = tmp;
             me = getProfile(id);
             return "home";
         }
